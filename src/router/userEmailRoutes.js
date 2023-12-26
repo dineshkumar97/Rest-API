fa7b60adb398fed0ce0f5cb313769f2bc4bc8d42
dineshkumar97 <dineshkumarppn07@gmail.com>
2023-12-26 23:49:59 +0530

    verify token

--- src/router/userEmailRoutes.js
+++ src/router/userEmailRoutes.js
@@ -4,7 +4,7 @@ const userEmailController = require('../controllers/userEmailController');
 const verifyToken = require('../middleware/verifytoken');
 
 // router.get('/getUserDetails',verifyToken, userEmailController.getUserDetails);
-router.get('/getUserDetails', userEmailController.getUserDetails);
+router.get('/getUserDetails', verifyToken,userEmailController.getUserDetails);
 router.get('/:idUser', userEmailController.getParticularUser);
 router.post('/create', userEmailController.createUser);
 router.post('/authenticate', userEmailController.authenticate);
